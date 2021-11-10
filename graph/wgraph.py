4ac915d8aacaacd5f02b27f47735a5c48b5fa2e2
Felipe Timbo <timbo.felipe@gmail.com>
2021-11-10 20:02:42 -0300

    adding types of edges in weighted graphs

--- graph/wgraph.py
+++ graph/wgraph.py
@@ -5,8 +5,6 @@ import numpy as np
 
 class WGraph(object):
 
-    PERCENTILES = np.arange(10, 101,10)
-
     def __init__(self, path=None, G=None, prune=None):
         if path is not None:
             self.G = gt.load_graph(path)
@@ -31,7 +29,8 @@ class WGraph(object):
         return np.average(self.degrees()) 
 
     def degrees_percentiles(self):
-        return np.percentile(self.degrees(), self.PERCENTILES).astype(int)   
+        deciles = np.arange(10, 101,10)
+        return np.percentile(self.degrees(), deciles).astype(int)   
 
     def edges_w(self):
         return self.ep.ew.fa.astype(int)
@@ -42,8 +41,60 @@ class WGraph(object):
     def avg_edges_w(self):
         return np.average(self.edges_w())     
 
+    def node_strength(self, v_id):
+        edges_v = self.G.get_out_edges(v_id, [self.G.ep.ew] )
+        return np.sum(edges_v[:,2]) 
+
+    def node_strengths(self):
+        return gt.incident_edges_op(self.G, "out", "sum", self.G.ep.ew).fa.astype(int)
+
     def edges_w_percentiles(self):
-        return np.percentile(self.edges_w(), self.PERCENTILES).astype(int)   
+        deciles = np.arange(10, 101,10)
+        return np.percentile(self.edges_w(), deciles).astype(int)   
+
+    def optins(self):
+        optins_mask = self.vp.optin.fa.astype(bool)
+        return self.G.get_vertices()[optins_mask]  
+
+    def optouts(self):
+        optouts_mask = ~self.vp.optin.fa.astype(bool)
+        return self.G.get_vertices()[optouts_mask]
+
+    def edges_without_in_in(self):
+        edges = self.G.get_edges()
+        sources = edges[:,0]
+        sources_optin = self.G.vp.optin.a[sources].astype(bool)
+        targets = edges[:,1]
+        targets_optin = self.G.vp.optin.a[targets].astype(bool)
+        not_optin_edges_mask = np.logical_or(np.invert(sources_optin), np.invert(targets_optin))
+        return not_optin_edges_mask
+
+    def edges_in_out(self):
+        edges = self.G.get_edges()
+        sources = edges[:,0]
+        sources_optin = self.G.vp.optin.a[sources].astype(bool)
+        targets = edges[:,1]
+        targets_optin = self.G.vp.optin.a[targets].astype(bool) 
+        optinout_edges_mask = np.logical_and(np.invert(sources_optin), targets_optin) | np.logical_and(sources_optin, np.invert(targets_optin))
+        return optinout_edges_mask
+
+    def edges_out_out(self):
+        edges = self.G.get_edges()
+        sources = edges[:,0]
+        sources_optin = self.G.vp.optin.a[sources].astype(bool)
+        targets = edges[:,1]
+        targets_optin = self.G.vp.optin.a[targets].astype(bool)
+        optout_edges_mask = np.invert(sources_optin) & np.invert(targets_optin)
+        return optout_edges_mask
+
+    def edges_in_in(self):
+        edges = self.G.get_edges()
+        sources = edges[:,0]
+        sources_optin = self.G.vp.optin.a[sources].astype(bool)
+        targets = edges[:,1]
+        targets_optin = self.G.vp.optin.a[targets].astype(bool)
+        optin_edges_mask = sources_optin & targets_optin
+        return optin_edges_mask
 
     def __getattr__(self, *args, **kwargs):
         return getattr(self.G, *args, **kwargs)
