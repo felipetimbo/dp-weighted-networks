70bb7fb5099578363a339eee4ad86cf16cfd4be2
Felipe Timbo <timbo.felipe@gmail.com>
2021-11-10 20:03:25 -0300

    results based on deciles

--- results_decile.py
+++ results_decile.py
@@ -39,75 +39,85 @@ class ResultsDPWeightedNets():
                     ego_metrics_true = {}
                     sets_ego_metric_1 = {}
                     sets_ego_metric_2 = {}
+                    sets_ego_metric_3 = {}
 
                     errors_1 = {} # approach 1
                     errors_2 = {} # approach 2
+                    errors_3 = {} # approach 2
 
                     for ego_metric in self.ego_metrics:
                         ego_metrics_true[ego_metric] = egocentric_metrics.calculate(g, ego_metric )
-                        # errors_1[ego_metric] = {}
-                        # errors_2[ego_metric] = {}
-
-                        # for error_metr in self.error_met: 
-                        #     errors_1[ego_metric][error_metr] = {}
-                        #     errors_2[ego_metric][error_metr] = {}
-
-                        #     for i in range(self.divide_data_into):
-                        #         errors_1[ego_metric][error_metr][i] = []
-                        #         errors_2[ego_metric][error_metr][i] = []
                     
                     for e in self.es:
                         utils.log_msg('*************** e = ' + str(e) + ' ***************')
                     
                         errors_list_1 = {} # approach 1
                         errors_list_2 = {} # approach 2
+                        errors_list_3 = {} # approach 3
 
                         for ego_metric in self.ego_metrics:
                             errors_1[ego_metric] = {}
                             errors_2[ego_metric] = {}
+                            errors_3[ego_metric] = {}
                             errors_list_1[ego_metric] = {}
                             errors_list_2[ego_metric] = {}
+                            errors_list_3[ego_metric] = {}
 
                             for error_metr in self.error_met: 
                                 errors_1[ego_metric][error_metr] = {}
                                 errors_2[ego_metric][error_metr] = {}
+                                errors_3[ego_metric][error_metr] = {}
                                 errors_list_1[ego_metric][error_metr] = {}
                                 errors_list_2[ego_metric][error_metr] = {}
+                                errors_list_3[ego_metric][error_metr] = {}
 
                                 for i in range(self.divide_data_into):
                                     errors_1[ego_metric][error_metr][i] = []
                                     errors_2[ego_metric][error_metr][i] = []
+                                    errors_3[ego_metric][error_metr][i] = []
                                     errors_list_1[ego_metric][error_metr][i] = []
                                     errors_list_2[ego_metric][error_metr][i] = []
+                                    errors_list_3[ego_metric][error_metr][i] = []
                                     
                         for r in range(self.runs):    
                             path_g1 = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'data', dataset, 'exp', 'graph_perturbed_%s_ins%s_e%s_r%s_baseline.graphml' % ( optin_method, optin_perc, e, r )))
                             g1 = WGraph(path_g1)
 
-                            path_g2 = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'data', dataset, 'exp', 'graph_perturbed_%s_ins%s_e%s_r%s_local.graphml' % ( optin_method, optin_perc, e, r )))
+                            path_g2 = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'data', dataset, 'exp', 'graph_perturbed_%s_ins%s_e%s_r%s_global_ds.graphml' % ( optin_method, optin_perc, e, r )))
                             g2 = WGraph(path_g2)
+
+                            path_g3 = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'data', dataset, 'exp', 'graph_perturbed_%s_ins%s_e%s_r%s_local.graphml' % ( optin_method, optin_perc, e, r )))
+                            g3 = WGraph(path_g3)
 
                             for ego_metr in self.ego_metrics:
                                 ego_metric_pred_1 = egocentric_metrics.calculate(g1, ego_metr)
                                 ego_metric_pred_2 = egocentric_metrics.calculate(g2, ego_metr)
+                                ego_metric_pred_3 = egocentric_metrics.calculate(g3, ego_metr)
                             
                                 pos_ego_metric_1 = np.argsort(ego_metrics_true[ego_metr])
                                 sets_ego_metric_1[ego_metr] = np.array_split(pos_ego_metric_1, self.divide_data_into)
                                 pos_ego_metric_2 = np.argsort(ego_metrics_true[ego_metr])
                                 sets_ego_metric_2[ego_metr] = np.array_split(pos_ego_metric_2, self.divide_data_into)
+                                pos_ego_metric_3 = np.argsort(ego_metrics_true[ego_metr])
+                                sets_ego_metric_3[ego_metr] = np.array_split(pos_ego_metric_3, self.divide_data_into)
 
                                 for i in range(self.divide_data_into):
                                     ego_metric_pred_1_i = ego_metric_pred_1[sets_ego_metric_1[ego_metr][i]]
                                     ego_metric_pred_2_i = ego_metric_pred_2[sets_ego_metric_2[ego_metr][i]]
+                                    ego_metric_pred_3_i = ego_metric_pred_3[sets_ego_metric_3[ego_metr][i]]
 
                                     for error_metr in self.error_met: 
                                         error_1 = error_metrics.calculate( error_metr, ego_metrics_true[ego_metr][sets_ego_metric_1[ego_metr][i]], ego_metric_pred_1_i)                   
                                         errors_list_1[ego_metr][error_metr][i].append(error_1)
-                                        utils.log_msg('g1 global %s %s = %s' % ( error_metr, ego_metr, error_1 ) )
+                                        utils.log_msg('g1 base %s %s = %s' % ( error_metr, ego_metr, error_1 ) )
 
                                         error_2 = error_metrics.calculate( error_metr, ego_metrics_true[ego_metr][sets_ego_metric_2[ego_metr][i]], ego_metric_pred_2_i)                   
                                         errors_list_2[ego_metr][error_metr][i].append(error_2)
-                                        utils.log_msg('g2 local %s %s = %s' % ( error_metr, ego_metr, error_2 ) )
+                                        utils.log_msg('g2 global %s %s = %s' % ( error_metr, ego_metr, error_2 ) )
+
+                                        error_3 = error_metrics.calculate( error_metr, ego_metrics_true[ego_metr][sets_ego_metric_3[ego_metr][i]], ego_metric_pred_3_i)                   
+                                        errors_list_3[ego_metr][error_metr][i].append(error_3)
+                                        utils.log_msg('g3 local %s %s = %s' % ( error_metr, ego_metr, error_3 ) )
 
                         for ego_metr in self.ego_metrics:
                             for error_metr in self.error_met:
@@ -118,16 +128,22 @@ class ResultsDPWeightedNets():
                                     ego_metric_mean_2 = np.mean( errors_list_2[ego_metr][error_metr][i] )
                                     errors_2[ego_metr][error_metr][i].append(ego_metric_mean_2)  
 
+                                    ego_metric_mean_3 = np.mean( errors_list_3[ego_metr][error_metr][i] )
+                                    errors_3[ego_metr][error_metr][i].append(ego_metric_mean_3)  
+
                         x = list(range(1,divide_data_into + 1))
 
-                        legends = ['global approach', 
-                                    'local approach ' ] 
+                        legends = ['global', 
+                                    'global + DS',
+                                    'local ' ] 
                         
                         for ego_metr in self.ego_metrics:
                             for error_metr in self.error_met: 
                                 y = []
                                 y.append(errors_1[ego_metr][error_metr].values())
                                 y.append(errors_2[ego_metr][error_metr].values())
+                                y.append(errors_3[ego_metr][error_metr].values())
+                                
                                 path_result = "./data/%s/results/result_%s_%s_%s_%s_e%s_div%s.png" % ( dataset, optin_method, optin_perc, ego_metr, error_metr, e, self.divide_data_into) 
                                 graphics.line_plot2(np.array(x), np.array(y), xlabel='decile', ylabel= error_metr, ylog=False, line_legends=legends, path=path_result)                                
                                 # path_result2 = "./data/%s/results/result_%s_%s_%s_%s_e%s_div%s_logscale.png" % ( dataset, optin_method, optin_perc, ego_metr, error_metr, e, self.divide_data_into) 
@@ -136,11 +152,11 @@ class ResultsDPWeightedNets():
 
 if __name__ == "__main__":
     datasets_names = [
-                    # 'high-school-contacts',
-                    # 'copenhagen-interaction',
+                    'high-school-contacts' ,
+                    'copenhagen-interaction']
                     # 'reality-call', 
                     # 'contacts-dublin',
-                    'digg-reply' ]
+                    # 'digg-reply' ]
                     # 'wiki-talk',
                     # 'sx-stackoverflow']
 
@@ -151,25 +167,25 @@ if __name__ == "__main__":
 
     error_met = ['mre']
 
-    ego_metrics = [ 'sum_of_2_hop_edges',
-                     'degree',
-                     'node_strength',
-                     'node_edges_weight_avg',  
+    ego_metrics = [ 
+                    # 'sum_of_2_hop_edges',
+                    #  'degree',
+                    #  'node_strength',
+                    #  'node_edges_weight_avg',  
+                    #  'density_w']
                     #  'density', 
-                    #  'num_edges_in_alters' ]
-                    # 'density',
-                     'density_w',
+                    #  'num_edges_in_alters' ]                    
                     # 'm',
                     # 'total_w',
                     # 'edges_w',
-                    #  'degree_all',
-                    #  'node_strength_all', 
-                    #  'node_edges_weight_avg_all', 
-                    # 'num_edges_in_alters_all',
-                    #  'sum_of_2_hop_edges_all',
+                      'degree_all',
+                      'node_strength_all', 
+                      'node_edges_weight_avg_all', 
+                      'num_edges_in_alters_all',
+                      'sum_of_2_hop_edges_all']
                     # 'ego_betweenness',
                     # 'ego_betweenness_all',
-                     'ego_betweenness_w']
+                    #  'ego_betweenness_w']
                     # 'page_rank',
                     # 'page_rank_all']
 
