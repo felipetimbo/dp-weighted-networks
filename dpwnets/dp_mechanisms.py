6bdecf0a524ace85ac825ca8cec54a9f14e1e1eb
Felipe Timbo <timbo.felipe@gmail.com>
2023-01-09 21:52:18 -0300

    refactoring mechanisms because new pre proc.

--- dpwnets/dp_mechanisms.py
+++ dpwnets/dp_mechanisms.py
@@ -1,10 +1,10 @@
 import numpy as np
 import mpmath 
+import multiprocessing
 
+from dpwnets import utils
 from random import randrange
 from scipy.linalg import convolution_matrix
-
-np.random.seed(1)
 
 variance = 100000
 
@@ -36,73 +36,105 @@ def geometric_mechanism(arr, eps, sensitivity=1):
     noisy_arr = arr + z
     return np.array(noisy_arr)
 
-def log_laplace_mechanism(arr, eps, sensitivity):
+def log_laplace_mechanism(arr, eps, sensitivity=2):
     lap = np.random.laplace(loc=0, scale=sensitivity/eps, size=len(arr))
     #  noisy_arr = np.around(np.clip( np.log(arr) + lap, 1, None )).astype(int)
     noisy_arr = np.around(np.clip( arr * np.exp(lap) , 1, max(arr) )).astype(int)  # https://arxiv.org/pdf/2101.02957.pdf
 
     return np.array(noisy_arr)
 
-def get_levels_size(arr):
-    m = len(arr)
-    non_zero_arr = np.array(arr)[np.nonzero(arr)[0]]
-    levels_len = 100000
-    
-    levels_size_up = []
-    levels_size_down = [ mpmath.mpf(1.0)]
-    
-    for l in range(levels_len+1):
+def find_point_of_decreasing(m, e):
+    # m = len(arr)
+
+    size_level_l_before = 0
+    for l in range(37235520800000, 100000000000000):
+        if l % 100000 == 0:
+            utils.log_msg('level: %s' % str(l))
         size_level_l_up = mpmath.binomial(l+m-1,m-1)
-        levels_size_up.append(size_level_l_up)
+        size_level_l_up_exp = mpmath.exp( mpmath.fprod( [mpmath.mpf(e), mpmath.fneg(mpmath.mpf(l)), mpmath.mpf(0.5)]) )
+        size_level_l = mpmath.fmul( size_level_l_up_exp, size_level_l_up )
+        if size_level_l < size_level_l_before:
+            break
+        else: 
+            size_level_l_before = size_level_l
+    
+    return l
 
-        if l < len(non_zero_arr):
-            w = arr[l] # weight of element at position l 
-            num_columns_of_levels_size_down = len(levels_size_down) + w 
-            a = convolution_matrix(levels_size_down, num_columns_of_levels_size_down, mode='valid')
-            levels_size_down = a.sum(axis=0)
+def get_levels_size(m, levels_range, num_threads=1):
+    # m = len(arr)
+    # non_zero_arr = np.array(arr)[np.nonzero(arr)[0]]
+    
+    # levels_size_up = []
+    # levels_size_down = [ mpmath.mpf(1.0)]
+    
+    range_l = np.array_split(list(range(levels_range[0], levels_range[1])), num_threads)
 
-        # else:
-        #     levels_size_down = np.append(levels_size_down, 0)
+    manager = multiprocessing.Manager()
+    levels_size_multiprocessing = manager.dict() 
 
-    if len(levels_size_down) < len(levels_size_up):
-        levels_size_down = np.append(levels_size_down, [0] * (len(levels_size_up) - len(levels_size_down)))
-    elif len(levels_size_down) > len(levels_size_up):
-        # levels_size_up = np.append(levels_size_up, [0] * (len(levels_size_down) - len(levels_size_up)))
-        levels_size_down = levels_size_down[:levels_len] #np.append(levels_size_up, [0] * (len(levels_size_down) - len(levels_size_up)))
+    threads = []
+    for i in range(num_threads):
+        t = multiprocessing.Process( target = compute_levels_size_parallel, args =( range_l[i], m, levels_size_multiprocessing ))
+        t.start()
+        threads.append(t)
+        
+    for t in threads:    
+        t.join()
 
-    levels_size = levels_size_up + levels_size_down
-    return levels_size_up
-    # descomentar bibliotecas
+    utils.log_msg('transforming runs in a dictionary...')
+    levels_size_dict = dict(levels_size_multiprocessing) 
 
-def compute_probability_mass(levels_size, e):
-    # prob_mass = []
-    # total_mass = 0
+    utils.log_msg('sorting levels...')
+    levels_size = np.array([levels_size_dict[key] for key in sorted(levels_size_dict.keys())])
+
+
+    #     if l < len(non_zero_arr):
+    #         w = arr[l] # weight of element at position l 
+    #         num_columns_of_levels_size_down = len(levels_size_down) + w 
+    #         a = convolution_matrix(levels_size_down, num_columns_of_levels_size_down, mode='valid')
+    #         levels_size_down = a.sum(axis=0)
+
+    # if len(levels_size_down) < len(levels_size_up):
+    #     levels_size_down = np.append(levels_size_down, [0] * (len(levels_size_up) - len(levels_size_down)))
+    # elif len(levels_size_down) > len(levels_size_up):
+    #     levels_size_down = levels_size_down[:levels_len] #np.append(levels_size_up, [0] * (len(levels_size_down) - len(levels_size_up)))
+
+    # levels_size = levels_size_up # + levels_size_down
+    return levels_size
+
+def compute_probability_mass(levels_range, levels_size, e):
 
     neg = np.vectorize(mpmath.fneg)
     mul = np.vectorize(mpmath.fmul)
     exp = np.vectorize(mpmath.exp)
 
-    sequence = mpmath.arange(1, len(levels_size))
+    utils.log_msg('computing probability mass...')
+    sequence = mpmath.arange(levels_range[0], levels_range[1])
     sequence_negatives = neg(sequence)
     sequence_negatives_times_e = mul(sequence_negatives, mpmath.mpf(e))
     sequence_negatives_div_two = mul(sequence_negatives_times_e, mpmath.mpf(0.5))
     exponential = exp( sequence_negatives_div_two )
 
-    prob_mass = exponential * levels_size[1:]
-    
-    return prob_mass
+    prob_mass = exponential * levels_size
 
-def exponential_mechanism(prob_mass):
-    # div = np.vectorize(mpmath.fdiv)
-
+    utils.log_msg('normalizing pr mass...')
     sum_prob_mass = mpmath.fsum(prob_mass)
-    # normalized_prob_mass = div(prob_mass, sum_prob_mass)
-
     normalized_prob_mass = []
     for mass in prob_mass:
         normalized_prob_mass.append(mpmath.fdiv(mass, sum_prob_mass))
+    
+    return normalized_prob_mass
 
-    return np.random.choice(a=len(prob_mass), size=1, p=normalized_prob_mass), normalized_prob_mass
+def exponential_mechanism(levels_range, normalized_prob_mass):
+    return np.random.choice(list(range(levels_range[0], levels_range[1])), size=1, p=normalized_prob_mass)[0]
+
+def compute_levels_size_parallel(levels_range, m, levels_size_mult):
+
+    for l in range(levels_range[0], levels_range[-1]+1):
+        size_level_l_up = mpmath.binomial(l+m-1,m-1)
+        levels_size_mult[l] = size_level_l_up
+
+    utils.log_msg('thread finished' )
 
 if __name__ == "__main__":
     arr = [ 3, 1, 2, 0, 0, 0 ]
